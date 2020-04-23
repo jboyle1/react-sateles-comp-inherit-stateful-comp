4c6eee56aa580b32c8790ea354fcbf45bf377d83
Jed Boyle <jedboyle01@gmail.com>
2020-04-23 12:40:15 +0100

    Updates

---
+++ precache-manifest.24f39efb68e79d71227dfce5ec39a7cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a93216d353c394f56aa0bb32b29016a",
+    "url": "/react-sateless-comp-inherit-stateful-comp/index.html"
+  },
+  {
+    "revision": "0175c32019e5b25067de",
+    "url": "/react-sateless-comp-inherit-stateful-comp/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "5cef56d9302c33686bf8",
+    "url": "/react-sateless-comp-inherit-stateful-comp/static/js/2.3ef7c37c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-sateless-comp-inherit-stateful-comp/static/js/2.3ef7c37c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0175c32019e5b25067de",
+    "url": "/react-sateless-comp-inherit-stateful-comp/static/js/main.f71a4643.chunk.js"
+  },
+  {
+    "revision": "ab4e864bde68148ce001",
+    "url": "/react-sateless-comp-inherit-stateful-comp/static/js/runtime-main.d9262de6.js"
+  }
+]);
