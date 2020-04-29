acaca974c9d8660e70689284956c5596ac06eaa3
JED BOYLE <40360931+jboyle1@users.noreply.github.com>
2020-04-29 12:00:21 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class Parent extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { name: 'Jed' };
+    this.state = { name: 'Frarthur' };
 
     this.changeName = this.changeName.bind(this);
   }
